d73e7260c8f5e47993b58fb4380a4128016995d4
Ash Tappin <ashtappin1592@hotmail.com>
2018-06-05 15:18:27 +0100

    Add (spring) bootstrapped project

---
+++ src/main/java/tappin/slackbots/lunchbot/LunchbotApplication.java
@@ -0,0 +1,12 @@
+package tappin.slackbots.lunchbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchbotApplication.class, args);
+	}
+}
